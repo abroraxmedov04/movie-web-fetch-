62b5bcc64108df4581fb5cfce1ebf8cd9c4a226c
unknown <abrordevaxmedov@gmail.com>
2024-05-16 18:00:37 +0300

    http problem

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://www.omdbapi.com/?apikey=feac457b&s=";
+const BASE_URL = "https://www.omdbapi.com/?apikey=feac457b&s=";
 
 const elForm = document.querySelector(".form");
 const elInput = document.querySelector(".input");
